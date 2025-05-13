0f4262afd9f885648be0dabac65b8eb31e1d2b7e
raineer <raineerdelarita@gmail.com>
2025-05-13 22:03:05 +0800

    cron

--- src/server.js
+++ src/server.js
@@ -6,6 +6,13 @@ require('dotenv').config({
 
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
+const cron = require('node-cron');
+
+// Daily cron job at midnight
+cron.schedule('0 0 * * *', () => {
+  console.log('Running daily membership status update...');
+  //updateMembershipStatuses();
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
