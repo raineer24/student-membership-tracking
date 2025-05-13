649ff47b01606daf7a952f6d30b7b320500dbdae
raineer <raineerdelarita@gmail.com>
2025-05-13 23:49:32 +0800

    payments

--- src/cron/updateMembershipStatuses.js
+++ src/cron/updateMembershipStatuses.js
@@ -1,28 +1,28 @@
-const prisma = require("../config/db");
-const {
-  checkPaymentStatus,
-  calculateDueDate,
-} = require("../utils/membershipUtils");
+// const prisma = require("../config/db");
+// const {
+//   checkPaymentStatus,
+//   calculateDueDate,
+// } = require("../utils/membershipUtils");
 
-const updateMembershipStatuses = async () => {
-  try {
-    const memberships = await prisma.membership.findMany();
-    for (const membership of memberships) {
-      const dueDate = calculateDueDate(membership.startDate, membership.type);
-      const status = checkPaymentStatus(dueDate);
-      if (status !== membership.paymentStatus) {
-        await prisma.membership.update({
-          where: { id: membership.id },
-          data: { paymentStatus: status },
-        });
-        console.log(
-          `Updated membership ID ${membership.id} to status: ${status}`
-        );
-      }
-    }
-  } catch (error) {
-    console.error("Error updating membership statuses:", error);
-  }
-};
+// const updateMembershipStatuses = async () => {
+//   try {
+//     const memberships = await prisma.membership.findMany();
+//     for (const membership of memberships) {
+//       const dueDate = calculateDueDate(membership.startDate, membership.type);
+//       const status = checkPaymentStatus(dueDate);
+//       if (status !== membership.paymentStatus) {
+//         await prisma.membership.update({
+//           where: { id: membership.id },
+//           data: { paymentStatus: status },
+//         });
+//         console.log(
+//           `Updated membership ID ${membership.id} to status: ${status}`
+//         );
+//       }
+//     }
+//   } catch (error) {
+//     console.error("Error updating membership statuses:", error);
+//   }
+// };
 
-module.exports = updateMembershipStatuses;
+// module.exports = updateMembershipStatuses;
--- src/server.js
+++ src/server.js
@@ -6,14 +6,14 @@ require('dotenv').config({
 
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
-const updateMembershipStatuses = require('./cron/updateMembershipStatuses');
-const cron = require('node-cron');
+//const updateMembershipStatuses = require('./cron/updateMembershipStatuses');
+//const cron = require('node-cron');
 
 // Daily cron job at midnight
-cron.schedule('0 0 * * *', () => {
-  console.log('Running daily membership status update...');
-  updateMembershipStatuses();
-});
+// cron.schedule('0 0 * * *', () => {
+//   console.log('Running daily membership status update...');
+//   updateMembershipStatuses();
+// });
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
