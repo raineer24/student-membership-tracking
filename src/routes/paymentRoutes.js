58aa36dbceaae74ef093a17931251c5bbdecdf25
raineer <raineerdelarita@gmail.com>
2025-05-13 21:22:21 +0800

    payment routes

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,4 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const paymentCtrl = require('../controllers/paymentController');
-const { verifyToken, isAdmin } = require('../middleware/authJwt');
+const paymentCtrl = require("../controllers/paymentController");
+const { verifyToken, isAdmin } = require("../middleware/authJwt");
+
+router.use(verifyToken);
+router.get("/me", paymentCtrl.getMyPayments);
+router.get("/", isAdmin, paymentCtrl.getAllPayments);
+router.post("/:memberId", isAdmin, paymentCtrl.addPayment);
+
+module.exports = router;
