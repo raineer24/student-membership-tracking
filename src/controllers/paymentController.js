c7b72b9ec287f580efcbfae72b0f6806a046d335
raineer <raineerdelarita@gmail.com>
2025-05-13 20:18:46 +0800

    payments controllers

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,2 +1,14 @@
 const prisma = require('../config/db');
 const AppError = require('../utils/AppError');
+
+exports.getAllPayments = async (req, res, next) => {
+
+}
+
+exports.getMyPayments = async (req, res, next) => {
+
+}
+
+exports.addPayment = async (req, res, next) => {
+    
+}
