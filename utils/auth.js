9de0dc4d7e807354172b9716d4fa682abf79815b
raineer <raineerdelarita@gmail.com>
2025-05-15 15:27:54 +0800

    authorze

--- utils/auth.js
+++ utils/auth.js
@@ -1,12 +1,20 @@
-const jwt = require("jsonwebtoken");
+// pages/api/utils/auth.js
 
-module.exports.default = async function authenticate(req) {
-  const auth = req.headers.authorization;
-  if (!auth) throw new Error('No token');
-  const token = auth.split(' ')[1];
+const jwt = require('jsonwebtoken');
+
+function authenticate(req) {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) throw new Error('Authentication required');
   return jwt.verify(token, process.env.JWT_SECRET);
 }
 
-module.exports.default = async function authorize(user, role) {
-  if (user.role !== role) throw new Error('Forbidden');
+function authorizeRole(requiredRole, user) {
+  if (user.role !== requiredRole) {
+    throw new Error('Forbidden: Insufficient permissions');
+  }
 }
+
+module.exports = {
+  authenticate,
+  authorizeRole,
+};
