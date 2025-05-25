8f5764aa0374ca38bed6a659c37c18282ae2be28
raineer <raineerdelarita@gmail.com>
2025-05-25 23:59:55 +0800

    404 not found

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
-import { useContext } from "react";
+import  AuthContext  from "../context/AuthContext";
+
 
 const ProtectedRoute = ({}) => {
   const { user, loading } = useContext(AuthContext);
