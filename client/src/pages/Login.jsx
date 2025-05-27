b65c4c7439b05f9f535945867ce4cc6ada66d6a7
raineer <raineerdelarita@gmail.com>
2025-05-27 22:08:09 +0800

    login vercel

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,7 +5,7 @@ import api from "../utils/api";
 
 const Login = () => {
   const [email, setEmail] = useState("");
-  const [password, setPasswprd] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -19,22 +19,12 @@ const Login = () => {
     }
   };
 
-  fetch('/api/auth/login', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    email: 'your-email@example.com',
-    password: 'your-password'
-  })
-})
-.then(res => res.json())
-.then(console.log);
 
   return (
     <form onSubmit={handleSubmit}>
         <h2>Login</h2>
         <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
-        <input type="password" placeholder="Password" value={password} onChange={(e) => setPasswprd(e.target.value)} required/>
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
         <button type="submit">Login</button>
     </form>
   )
