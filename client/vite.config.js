d09c75ac0c3d259b606c83fd84f4a9bb4c2da5a9
raineer <raineerdelarita@gmail.com>
2025-06-03 22:32:39 +0800

    tailwind css not working

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
     }
   },
   build: {
-    outDir: 'dist',  // Output to client/dist
-    emptyOutDir: true  // Clear the output directory
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  css: {
+    postcss: './postcss.config.js'
   }
 });
