0c81182fe2882d6e3bdf59e1a39edc45cca9ae5e
raineer <raineerdelarita@gmail.com>
2025-06-03 17:25:31 +0800

    dashboard page and appjsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/AuthContext";
 import Login from "./pages/Login";
 import MembershipPage from "./pages/MembershipPage";
+import DashboardPage from "./components/DashboardPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 // Custom Redirect Component
@@ -20,6 +21,9 @@ const HomeRedirect = () => {
     } else if (user.role === "STUDENT") {
       console.log("Redirecting to /membership");
       navigate("/membership", { replace: true });
+       } else if (user.role === "ADMIN") {
+      console.log("Redirecting to /dashboard");
+      navigate("/dashboard", { replace: true });
     } else {
       console.log("Redirecting to /login (unexpected role)");
       navigate("/login", { replace: true });
@@ -40,6 +44,14 @@ function App() {
           path="/membership"
           element={
             <ProtectedRoute allowedRoles={["STUDENT"]}>
+              <MembershipPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["ADMIN"]}>
               <MembershipPage />
             </ProtectedRoute>
           }
---
+++ client/src/components/DashboardPage.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../context/AuthContext';
+
+export default function DashboardPage() {
+    return (
+        <h2>Dashboard page</h2>
+    )
+}
