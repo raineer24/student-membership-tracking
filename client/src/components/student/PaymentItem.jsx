b7f57e9dce0f4a342503c0e58e2798da12a37f55
raineer <raineerdelarita@gmail.com>
2025-06-06 21:19:59 +0800

    student project structure

---
+++ client/src/components/student/PaymentItem.jsx
@@ -0,0 +1,17 @@
+const PaymentItem = ({ payment}) => {
+    const formatDate = (dateString) => {
+
+    }
+
+    const formatAmount = (amount) => {
+
+    }
+
+    const getStatusClass = (status) => {
+
+    };
+
+    return ()
+}
+
+export default PaymentItem;
