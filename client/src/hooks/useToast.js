af57b50f3bdfa861298feb2e4ff721a0e03e6e5f
raineer <raineerdelarita@gmail.com>
2025-06-19 16:48:42 +0800

    return showerror ;

--- client/src/hooks/useToast.js
+++ client/src/hooks/useToast.js
@@ -1,1 +1,21 @@
 import { useState } from "react";
+
+export const useToast = () => {
+    const [toast, setToast] = useState(null);
+
+    const showSuccess = (message) => {
+        setToast({message, type: 'sucess', id: Date.now()});
+
+        
+    }
+
+    const showError = (message) => {
+            setToast({message, type: 'error', id: Date.now()});
+        }
+
+        const hideToast = () => {
+            setToast(null);
+        };
+
+        return { toast, showSuccess, showError, hideToast}
+}
