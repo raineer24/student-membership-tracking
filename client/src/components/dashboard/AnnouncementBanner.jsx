531dbcd00626e5bd488d697bdcfc377619699aba
raineer <raineerdelarita@gmail.com>
2025-08-19 17:51:08 +0800

    announcementBanner

---
+++ client/src/components/dashboard/AnnouncementBanner.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useCallback } from 'react';
+
+const AnnouncementBanner = ({ announcements, onDismiss, onEdit}) => {
+    const [] = useState(new Set());
+};
+
+export default AnnouncementBanner;
